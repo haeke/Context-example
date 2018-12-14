86fd955777ca81caf75cf893524b0a1f1022fca1
Edwin Rivera <waehake@gmail.com>
2018-12-14 11:31:59 -0500

    Updates

---
+++ precache-manifest.ec75ba162a0a49284fed7f8a208f3d36.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "870e3609d3ee7a0b59d5",
+    "url": "/static/js/main.870e3609.chunk.js"
+  },
+  {
+    "revision": "4d4949a9a9f903a5cc4a",
+    "url": "/static/js/1.4d4949a9.chunk.js"
+  },
+  {
+    "revision": "1e509f39036f9ad8f0687160ad72577a",
+    "url": "/index.html"
+  }
+];
